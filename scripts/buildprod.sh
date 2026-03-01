f823402996f461ee956b3272b0fde71b8202f088
Sanghun Park <sanghunpark@Sanghuns-MacBook-Pro.local>
2026-03-01 19:34:55 +0100

    F: added cd

--- scripts/buildprod.sh
+++ scripts/buildprod.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o notely
+# 1. Build the binary for ARM64
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o notely .
+
+# 2. Build the Docker image using that binary
+docker build -t sanghunpark0628/notely:latest . --platform=linux/arm64
